1314a2feb256f99fd4ce13d0785004eebf590e2c
Gangadhar Reddy <133688@gangadhar.local>
2020-02-19 12:19:36 +0530

    add

--- README.md
+++
@@ -1,2 +0,0 @@
-# onBoarding
-on boarding
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-const gatewayEndPoint = "http://localhost:4200";
-export const environment = {
-    production: true,
-    BASE_API_URL: gatewayEndPoint,
-    appDetailUrl: "/assets/merchant.json",
-    actionViewDetailUrl: "/assets/actionView.json"
-};
